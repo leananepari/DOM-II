f5c627782908af7d433030c3eba78f029c880b24
Leana <leananeparidze.com>
2019-06-19 09:31:59 -0700

    removes unneccessary lines

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,18 +1,11 @@
 let red = document.querySelector('.block--red');
-console.log(red)
 let blue = document.querySelector('.block--blue');
 let green = document.querySelector('.block--green');
 let pink = document.querySelector('.block--pink');
 let gray = document.querySelector('.block--gray');
 
-let parent = document.querySelector('.blocks');
-parent.addEventListener('click', (event) => {
-  event.stopPropagation();
-})
-
 red.addEventListener('click', (event) => {
   event.preventDefault();
-  event.stopPropagation();
   let all = document.querySelectorAll('.blocks div');
   all.forEach(elem => elem.removeAttribute('style'));
   console.log(all)
@@ -21,7 +14,6 @@ red.addEventListener('click', (event) => {
 
 blue.addEventListener('click', (event) => {
   event.preventDefault();
-  event.stopPropagation();
   let all = document.querySelectorAll('.blocks div');
   all.forEach(elem => elem.removeAttribute('style'));
   blue.style.order = '-1';
@@ -29,7 +21,6 @@ blue.addEventListener('click', (event) => {
 
 green.addEventListener('click', (event) => {
   event.preventDefault();
-  event.stopPropagation();
   let all = document.querySelectorAll('.blocks div');
   all.forEach(elem => elem.removeAttribute('style'));
   green.style.order = '-1';
@@ -37,7 +28,6 @@ green.addEventListener('click', (event) => {
 
 pink.addEventListener('click', (event) => {
   event.preventDefault();
-  event.stopPropagation();
   let all = document.querySelectorAll('.blocks div');
   all.forEach(elem => elem.removeAttribute('style'));
   pink.style.order = '-1';
@@ -45,7 +35,6 @@ pink.addEventListener('click', (event) => {
 
 gray.addEventListener('click', (event) => {
   event.preventDefault();
-  event.stopPropagation();
   let all = document.querySelectorAll('.blocks div');
   all.forEach(elem => elem.removeAttribute('style'));
   gray.style.order = '-1';
